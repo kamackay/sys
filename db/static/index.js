faf46e870f836e41e59aefebe27b6c0d2e0c74b8
mackayk <mackayk@NC45LT3TYQQF2>
2017-03-23 13:39:16 -0400

    can't add redundant machine name

--- db/static/index.js
+++ db/static/index.js
@@ -1,12 +1,14 @@
 var app = angular.module('module', ["ui.materialize"]);
 app.controller('controller', function ($scope, $http) {
+  // Pull from the database
   $scope.update = function () {
     $http.get('/get/').then(function (data) {
       $scope.machines = data.data;
     });
   };
+  // remove all of the edit flags from the data
   $scope.noEdit = function () {
-    for (var x = 0; x < $scope.machines.length; x++) $scope.machines[x].edit = false;
+    for (var x = 0; x < $scope.machines.length; x++) $scope.machines[x].edit = undefined;
   };
   window.setInterval(() => {
     if (document.getElementById('autoUpdate').checked) {
@@ -22,8 +24,9 @@ app.controller('controller', function ($scope, $http) {
       Materialize.toast("Saved to database", 2500, "rounded");
     });
   };
+  // Reserve A Machine
   $scope.reserve = function (machine, name) {
-    if (name === undefined) name = prompt('What is your name?', 'Machine Name')
+    if (name === undefined) name = prompt('What is your name?', 'Name')
     for (var i = 0; i < $scope.machines.length; i++) {
       if ($scope.machines[i].name === machine) {
         $scope.machines[i].available = false;
@@ -31,6 +34,7 @@ app.controller('controller', function ($scope, $http) {
       }
     }
   };
+  // Release A Machine
   $scope.release = function (machine) {
     for (var i = 0; i < $scope.machines.length; i++) {
       if ($scope.machines[i].name === machine) {
@@ -41,12 +45,20 @@ app.controller('controller', function ($scope, $http) {
   };
   $scope.add = function () {
     var machineName = prompt("Machine Name", "");
+    // Check to see if that machine name exists already
+    for (var x = 0; x < $scope.machines.length; x++) {
+      if ($scope.machines[x].name === machineName) {
+        Materialize.toast("Machine name exists already", 3000, "rounded");
+        return;
+      }
+    }
     $scope.machines.push({
       name: machineName,
       available: true,
       reservedBy: ""
     });
   };
+  // Delete a machine (The first one found with the given name)
   $scope.delete = function (machine) {
     for (var x = 0; x < $scope.machines.length; x++) {
       if ($scope.machines[x].name == machine) {
@@ -54,43 +66,6 @@ app.controller('controller', function ($scope, $http) {
         return;
       }
     }
-  };
-  $scope.edit = function (machine) {
-    const form = $("#editForm");
-    $.each(form.find(".machineName"), function (x, el) {
-      var e = $(el);
-      if (el.tagName.toLowerCase() === "span") e.text(machine.name);
-      else if (el.tagName.toLowerCase() === "input") e.val(machine.name);
-      else console.log(e);
-    });
-    $.each(form.find(".machineType"), function (x, el) {
-      if (el.tagName.toLowerCase() === "input") $(el).val(machine.type);
-      else console.log(el);
-    });
-    $.each(form.find(".machineLocation"), function (x, el) {
-      if (el.tagName.toLowerCase() === "input") $(el).val(machine.location);
-      else console.log(el);
-    });
-    var submit = null;
-    submit = function () {
-      document.getElementById("editForm_submit").removeEventListener("onclick", submit);
-      for (var x = 0; x < $scope.machines.length; x++) {
-        if ($scope.machines[x].name === machine.name) {
-          $scope.machines[x].name = form.find("input.machineName").first().val();
-          $scope.machines[x].type = form.find("input.machineType").first().val();
-          $scope.machines[x].location = form.find("input.machineLocation").first().val();
-          console.log($scope.machines[x]);
-          $scope.save();
-          setTimeout($scope.update, 100);
-          break;
-        }
-      } /**/
-      document.getElementById("editForm").removeAttribute("style");
-    };
-    document.getElementById("editForm_submit").addEventListener("click", submit);
-    form.animate({
-      top: '25vh'
-    }, 1000);
   };
 });
 
