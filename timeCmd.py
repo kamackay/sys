451ef6ca15f47a6860f830debb82fbb03bb280c7
kamackay <kamackay@ncsu.edu>
2017-04-13 17:41:04 -0400

    added the tab back

--- timeCmd.py
+++ timeCmd.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
   startTime = datetime.now()
   popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
   for stdout_line in iter(popen.stdout.readline, ""):
-    log(stdout_line)
+    log("\t" + stdout_line)
   popen.stdout.close()
   return_code = popen.wait()
   endTime = datetime.now()
