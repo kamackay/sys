738938bb7c9edc4b1489702625925796d196a35e
mackayk <mackayk@NC45LT3TYQQF2>
2017-05-25 14:23:24 -0400

    logs again

--- db/static/logs/index.js
+++ db/static/logs/index.js
@@ -8,13 +8,39 @@ app.controller('controller', function ($scope, $http) {
     if (files.length == 0) alert("No Files selected, dummy");
     else {
       for (var i = 0; i < files.length; i++) {
+        const file = files.item(i);
         const reader = new FileReader();
-        reader.readAsText(files[i]);
+        reader.readAsText(file);
         reader.onload = function (e) {
           const logContents = e.target.result;
+          const $el = window.logOutputDiv;
+          var testInfo = {
+            result: -1,
+            time: -1,
+            asserts_host: [],
+            asserts_comms: [],
+            traceback: '',
+            fw_host: 'X_X',
+            fw_comms: 'X_X',
+            wfe: [],
+            find: {}
+          };
+          
         };
       }
     }
+  };
+
+  $scope.downloadInfo = function () {
+    const text = document.getElementById('logOutput').innerHTML;
+    const filename = 'parsed.html';
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
   };
 });
 
